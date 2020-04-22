deba2c2d0d1059689501ffdd78cb87e8108b665b
Heshvipatel17 <heshvipatel17@gmail.com>
2020-04-22 19:25:50 +0100

    hw

--- src/main/java/com/orangehrm/pages/HomePage.java
+++ src/main/java/com/orangehrm/pages/HomePage.java
@@ -1,23 +1,40 @@
 package com.orangehrm.pages;
 
 import com.orangehrm.utility.Utility;
-import org.openqa.selenium.By;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Reporter;
 
 public class HomePage extends Utility {
 
-    By orangeHrmLink = By.xpath("//img[@class='nav-logo']");
-    By free30DayTrialLink = By.xpath("//a[contains(@class,'btn-orange trial-btn pulse')]");
-    By contactSalesLink = By.xpath("//a[contains(@class,'btn-orange contact-ohrm')]");
+    private static final Logger log = LogManager.getLogger(HomePage.class.getName());
+
+    @FindBy(xpath = "//img[@class='nav-logo']")
+    WebElement _orangeHrmLink;
+
+    @FindBy(xpath = "//a[contains(@class,'btn-orange trial-btn pulse')]")
+    WebElement _free30DayTrialLink;
+
+    @FindBy(xpath = "//a[contains(@class,'btn-orange contact-ohrm')]")
+    WebElement _contactSalesLink;
 
     public void clickOnOrangeHrm() {
-        clickOnElement(orangeHrmLink);
+        Reporter.log("Clicking on OrangeHRM link "+ _orangeHrmLink.toString() + "<br>");
+        clickOnElement(_orangeHrmLink);
+        log.info("Clicking on OrangeHRM link "+ _orangeHrmLink.toString());
     }
 
     public void clickOnFree30DayTrialLink() {
-        clickOnElement(free30DayTrialLink);
+        Reporter.log(" Clicking on 30 day free trial "+ _free30DayTrialLink.toString() + "<br>");
+        clickOnElement(_free30DayTrialLink);
+        log.info(" Clicking on 30 day free trial "+ _free30DayTrialLink.toString());
     }
 
     public void clickOnContactSalesLink() {
-        clickOnElement(contactSalesLink);
+        Reporter.log(" Clicking on contact sales link "+ _contactSalesLink.toString() + "<br>");
+        clickOnElement(_contactSalesLink);
+        log.info(" Clicking on contact sales link "+ _contactSalesLink.toString());
     }
 }
--- src/test/java/com/orangehrm/testsuite/TestCase.java
+++ src/test/java/com/orangehrm/testsuite/TestCase.java
@@ -2,22 +2,31 @@ package com.orangehrm.testsuite;
 
 import com.orangehrm.pages.HomePage;
 import com.orangehrm.testbase.TestBase;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestCase extends TestBase {
-    HomePage homePage = new HomePage();
 
-    @Test(priority = 0, groups = {"Sanity","Regression"})
-    public void VerifyUserShouldNavigateToOrangeHrmPage(){
+    HomePage homePage;
+
+    @BeforeMethod(groups = {"Regression", "Smoke", "Sanity"})
+    public void setUp() {
+        homePage = new HomePage();
+    }
+
+    @Test(priority = 0, groups = {"Sanity", "Regression"})
+    public void VerifyUserShouldNavigateToOrangeHrmPage() {
         homePage.clickOnOrangeHrm();
     }
-    @Test(priority = 1, groups = {"Smoke","Regression"})
-    public void VerifyUserShouldClickOnFree30DayTrialLink(){
+
+    @Test(priority = 1, groups = {"Smoke", "Regression"})
+    public void VerifyUserShouldClickOnFree30DayTrialLink() {
         homePage.clickOnFree30DayTrialLink();
 
     }
-    @Test (priority = 2, groups = {"Regression"})
-    public void verifyUserShouldClickOnContactSalesLink(){
+
+    @Test(priority = 2, groups = {"Regression"})
+    public void verifyUserShouldClickOnContactSalesLink() {
         homePage.clickOnContactSalesLink();
 
     }
